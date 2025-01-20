ae384b5ebfd029ceb1344f7997ee0c566b0cc309
isakdevelop <daishi7462@naver.com>
2025-01-20 13:01:19 +0900

    Create - new Project

---
+++ src/main/java/com/fisher/api/FisherApplication.java
@@ -0,0 +1,13 @@
+package com.fisher.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FisherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FisherApplication.class, args);
+    }
+
+}
