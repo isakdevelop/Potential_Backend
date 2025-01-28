29ba4900e9c50f202afa4f99e60923417befe1f0
isakdevelop <daishi7462@naver.com>
2025-01-28 23:35:15 +0900

    Feat : Add error codes when not supported by the server

--- src/main/java/com/potential/api/common/enums/Error.java
+++ src/main/java/com/potential/api/common/enums/Error.java
@@ -11,7 +11,8 @@ public enum Error {
     BAD_REQUEST(HttpStatus.BAD_REQUEST.value(), "잘못된 형식입니다."),
     FORBIDDEN(HttpStatus.FORBIDDEN.value(), "접근 권한이 존재하지않습니다."),
     NOT_FOUND(HttpStatus.NOT_FOUND.value(), "존재하지 않는 값 입니다."),
-    CONFLICT(HttpStatus.CONFLICT.value(), "이미 중복된 값입니다.")
+    CONFLICT(HttpStatus.CONFLICT.value(), "이미 중복된 값입니다."),
+    UNPROCESSABLE_ENTITY(HttpStatus.UNPROCESSABLE_ENTITY.value(), "서버가 처리를 할 수 없는 데이터가 포함되었습니다.");
     ;
 
     private final int status;
