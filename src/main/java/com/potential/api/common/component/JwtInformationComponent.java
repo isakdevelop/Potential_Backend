8089e4d9a0d40011be601dd8ba4fb460e70d670e
isakdevelop <daishi7462@naver.com>
2025-01-28 23:36:00 +0900

    Feat : Adding to Method Components for Reusability

--- src/main/java/com/potential/api/common/component/JwtInformationComponent.java
+++ src/main/java/com/potential/api/common/component/JwtInformationComponent.java
@@ -1,14 +1,27 @@
 package com.potential.api.common.component;
 
+import com.potential.api.common.enums.Error;
+import com.potential.api.common.exception.PotentialException;
+import com.potential.api.model.User;
+import com.potential.api.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class JwtInformationComponent {
+    private final UserRepository userRepository;
+
     public String getUserIdFromJWT() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         return authentication.getName();
+    }
+
+    public User certificationUserJWT(String jwt) {
+        return userRepository.findById(getUserIdFromJWT())
+                .orElseThrow(() -> new PotentialException(com.potential.api.common.enums.Error.FORBIDDEN.getStatus(), Error.FORBIDDEN.getMessage()));
     }
 
 }
