0740449447c8dc259a3540f6c73a0f1ccc465cab
isakdevelop <daishi7462@naver.com>
2025-01-29 23:59:38 +0900

    Refactor : Apply builder patterns for maintenance and scalability

--- src/main/java/com/potential/api/dto/response/LoginResponseDto.java
+++ src/main/java/com/potential/api/dto/response/LoginResponseDto.java
@@ -1,21 +1,14 @@
 package com.potential.api.dto.response;
 
-import com.potential.api.common.enums.Role;
 import com.potential.api.dto.ResponseDto;
 import lombok.Getter;
+import lombok.experimental.SuperBuilder;
 
 @Getter
+@SuperBuilder
 public class LoginResponseDto extends ResponseDto {
     private final String accessToken;
     private final String refreshToken;
     private final int expirationTime;
     private final String role;
-
-    public LoginResponseDto(int status, String message, String accessToken, String refreshToken, int expirationTime, Role role) {
-        super(status, message);
-        this.accessToken = accessToken;
-        this.refreshToken = refreshToken;
-        this.expirationTime = expirationTime;
-        this.role = String.valueOf(role);
-    }
 }
