6f71c0c927346e54053e7c77f48f32b484970be6
isakdevelop <daishi7462@naver.com>
2025-01-30 00:00:58 +0900

    Refactor : Add SuperBuilder

--- src/main/java/com/potential/api/dto/ResponseDto.java
+++ src/main/java/com/potential/api/dto/ResponseDto.java
@@ -2,9 +2,11 @@ package com.potential.api.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.experimental.SuperBuilder;
 
 @Getter
 @AllArgsConstructor
+@SuperBuilder
 public class ResponseDto {
     private int status;
     private String message;
