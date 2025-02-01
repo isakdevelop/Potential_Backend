081428f59042e1b53c1c378ced535a1992396ec9
isakdevelop <daishi7462@naver.com>
2025-02-01 23:26:56 +0900

    Feat : Create Heart function in post

---
+++ src/main/java/com/potential/api/dto/request/PostToggleHeartRequestDto.java
@@ -0,0 +1,8 @@
+package com.potential.api.dto.request;
+
+import lombok.Getter;
+
+@Getter
+public class PostToggleHeartRequestDto {
+    private String postId;
+}
---
+++ src/main/java/com/potential/api/model/PostHeart.java
@@ -0,0 +1,26 @@
+package com.potential.api.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "post_heart")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+public class PostHeart extends BaseEntity {
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
